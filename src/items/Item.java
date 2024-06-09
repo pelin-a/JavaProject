02cebf08da7a46dedbb427a8d75cc1575f5ef4ea
Pelinsu Altun <pelinsualtun@gmail.com>
2024-06-09 14:05:16 +0200

    Item class created constructor, getter setters and volume method

--- src/items/Item.java
+++ src/items/Item.java
@@ -2,6 +2,61 @@ package items;
 
 public class Item {
 	
+	//private item attributes
+	private double length;
+	private double width;
+	private double depth;
+	private double weight;
 	
+	//constructor
+	public Item(double length, double width, double depth, double weight) {
+		this.length = length;
+		this.width= width;
+		this.depth= depth;
+		this.weight= weight;
+	}
+	// getters and setters
+	public double getLength() {
+		return length;
+	}
 
+	public void setLength(double length) {
+		this.length = length;
+	}
+
+	public double getWidth() {
+		return width;
+	}
+
+	public void setWidth(double width) {
+		this.width = width;
+	}
+
+	public double getDepth() {
+		return depth;
+	}
+
+	public void setDepth(double depth) {
+		this.depth = depth;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+	public double getItemVolume() {
+		double volume;
+		volume= this.length * this.weight * this.depth;
+		return volume;
+	}
+	
+	
+	
+	
+	
+	
 }
